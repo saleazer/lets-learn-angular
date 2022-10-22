b6e8b54890c61f4f1931e721854126fa38e23d80
Stephanie Leazer <Stephanie.Leazer@loves.com>
2022-10-22 17:32:14 -0500

    Add footer, change homepage, update Discord link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppHomeComponent } from './app-home/app-home.component';
+import { AngularResourcesComponent } from './angular-resources/angular-resources.component';
 
 const routes: Routes = [
-  { path: '**', component: AppHomeComponent }
+  { path: '**', component: AngularResourcesComponent }
 ];
 
 @NgModule({
