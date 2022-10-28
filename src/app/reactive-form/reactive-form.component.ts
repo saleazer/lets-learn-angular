8f5171d0ac798b5f84787b0c26e98c2fc1ed6aac
Stephanie Leazer <Stephanie.Leazer@loves.com>
2022-10-27 21:07:06 -0500

    Add form control for favColor

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -14,6 +14,7 @@ export class ReactiveFormComponent implements OnInit {
   ngOnInit(): void {
     this.testForm = this.fb.group({
       name: ['', Validators.required],
+      favColor: ['']
     })
   }
 
