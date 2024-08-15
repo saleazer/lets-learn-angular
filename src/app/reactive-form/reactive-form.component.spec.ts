b3a38f9b917d5953d9d018282f461f2c1ccd3191
Stephanie Leazer <Stephanie.Leazer@loves.com>
2024-08-15 14:09:58 -0500

    Add sample methods + unit tests to run StrykerJS against

--- src/app/reactive-form/reactive-form.component.spec.ts
+++ src/app/reactive-form/reactive-form.component.spec.ts
@@ -20,4 +20,28 @@ describe('ReactiveFormComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe("timesTwo", () => {
+    it("should return the input value multiplied by two", () => {
+      let results = component.timesTwo(2);
+
+      expect(results).toBe(4);
+    });
+  });
+
+  describe("timesFour", () => {
+    it("should return the input value multiplied by four", () => {
+      let results = component.timesFour(2);
+
+      expect(results).toBe(8);
+    });
+  });
+
+  describe("timesEight", () => {
+    it("should return the input value multiplied by eight", () => {
+      let results = component.timesEight(2);
+
+      expect(results).toBe(16);
+    });
+  });
 });
