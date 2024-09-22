debb3c3fbbf36b474b2e66b54dfeec13970fbd05
Stephanie Leazer <Stephanie.Leazer@loves.com>
2024-09-22 17:25:28 -0500

    Update config to stryke all component files

--- stryker.config.mjs
+++ stryker.config.mjs
@@ -2,7 +2,7 @@
 /** @type {import('@stryker-mutator/api/core').PartialStrykerOptions} */
 const config = {
   mutate: [
-    "src/**/*form.component.ts",
+    "src/**/*.component.ts",
     "!src/**/*.spec.ts",
     "!src/test.ts",
     "!src/environments/*.ts",
