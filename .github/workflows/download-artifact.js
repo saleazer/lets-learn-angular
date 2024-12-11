81f1ba7ffb80175016ecabde341fd69353acbfd0
Stephanie <Stephanie.Leazer@loves.com>
2024-12-11 09:00:43 -0600

    Big logic change for stryking + reset count back to 0

--- .github/workflows/download-artifact.js
+++ .github/workflows/download-artifact.js
@@ -25,136 +25,132 @@ async function getLatestSuccessfulRun() {
     });
     console.log(`getLatestSuccessfulRun Response status: ${response.status}`);
     const run = response.data.workflow_runs[0];
-    console.log(`latestSuccessfulRunId= ${run.id}`);
+    console.log(`latestSuccessfulRunId = ${run.id}`);
     return run ? run.id : null;
 }
 
 async function getArtifactId(runId) {
-  console.log(`gettingArtifactId for run#: ${runId}`);
-  const url = `https://api.github.com/repos/${OWNER}/${REPO}/actions/runs/${runId}/artifacts`;
-  const response = await axios.get(url, {
-    headers: {
-      Authorization: `token ${GITHUB_TOKEN}`
+    if (!runId) {
+      console.log('NULL runId provided, unable to get artifactId');
+      return null;
     }
-  });
+    console.log(`Valid runId provided, gettingArtifactId for run#: ${runId}`);
+    const url = `https://api.github.com/repos/${OWNER}/${REPO}/actions/runs/${runId}/artifacts`;
+    const response = await axios.get(url, {
+        headers: {
+          Authorization: `token ${GITHUB_TOKEN}`
+        }
+    });
 
-  console.log(`getArtifactId Response status: ${response.status}`);
-  const artifact = response.data.artifacts.find(artifact => artifact.name.includes(ARTIFACT_NAME));
-  if (artifact.name.includes('_')) {
-    previousCount = artifact.name.split('_')[1];
-  }
-  console.log(`artifactId= ${artifact.id}`);
-  return artifact ? artifact.id : null;
+    console.log(`getArtifactId Response status: ${response.status}`);
+    const artifact = response.data.artifacts.find(artifact => artifact.name.includes(ARTIFACT_NAME));
+    // if (artifact.name.includes('_')) {
+    //   previousCount = artifact.name.split('_')[1];
+    // }
+    console.log(`artifactId = ${artifact.id}`);
+    return artifact ? artifact.id : null;
 }
 
 async function downloadArtifact(artifactId) {
-  console.log(`downloadingArtifact: ${artifactId}`);
-  const url = `https://api.github.com/repos/${OWNER}/${REPO}/actions/artifacts/${artifactId}/zip`;
-  const response = await axios.get(url, {
-    headers: {
-      Authorization: `token ${GITHUB_TOKEN}`
-    },
-    responseType: 'arraybuffer'
-  });
+    if (!artifactId) {
+      console.log('NULL artifactId provided, unable to download artifact');
+      return null;
+    }
+    console.log(`Valid artifactId provided, downloadingArtifact: ${artifactId}`);
+    const url = `https://api.github.com/repos/${OWNER}/${REPO}/actions/artifacts/${artifactId}/zip`;
+    const response = await axios.get(url, {
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`
+      },
+      responseType: 'arraybuffer'
+    });
 
-  console.log(`downloadArtifact Response status: ${response.status}`);
-  const zipPath = path.join(__dirname, `${ARTIFACT_NAME}.zip`);
-  fs.writeFileSync(zipPath, Buffer.from(response.data));
-  // console.log(`zipped artifact path: ${zipPath}`);
-  return zipPath;
+    console.log(`downloadArtifact Response status: ${response.status}`);
+    const zipPath = path.join(__dirname, `${ARTIFACT_NAME}.zip`);
+    fs.writeFileSync(zipPath, Buffer.from(response.data));
+    console.log(`zipped artifact path: ${zipPath}`);
+    return zipPath;
 }
 
 function extractArtifact(zipPath) {
-    console.log(`extractingArtifact from: ${zipPath}`);
-  const zip = new AdmZip(zipPath);
-  const extractPath = path.join(__dirname, 'extracted_artifact');
-  zip.extractAllTo(extractPath, true);
-    // console.log(`extracted artifact path: ${extractPath}`);
-  return extractPath;
+    if (!zipPath) {
+      console.log('NULL zipPath provided, unable to extract artifact');
+      return null;
+    }
+    console.log(`Valid zipPath provided, extractingArtifact from: ${zipPath}`);
+    const zip = new AdmZip(zipPath);
+    const extractPath = path.join(__dirname, 'extracted_artifact');
+    zip.extractAllTo(extractPath, true);
+    console.log(`extracted artifact path: ${extractPath}`);
+    return extractPath;
+}
+
+async function getAllFiles(octokit, owner, repo, path) {
+    const { data: items } = await octokit.rest.repos.getContent({
+        owner,
+        repo,
+        path
+    });
+
+    let files = [];
+
+    for (const item of items) {
+        if (item.type === 'file') {
+            files.push(item.path);
+        } else if (item.type === 'dir') {
+            const subFiles = await getAllFiles(octokit, owner, repo, item.path);
+            files = files.concat(subFiles);
+        }
+    }
+
+    return files;
 }
 
 async function getComponentFiles() {
-    if (previousCount === 'complete') {
-        core.exportVariable('COMPONENT_COUNT', 'complete');
-        return;
-    }
     try {
         const { context } = github;
         const octokit = github.getOctokit(GITHUB_TOKEN);
-        
-        async function getAllFiles(octokit, owner, repo, path) {
-          const { data: items } = await octokit.rest.repos.getContent({
-              owner,
-              repo,
-              path
-          });
-      
-          let files = [];
-      
-          for (const item of items) {
-              if (item.type === 'file') {
-                  files.push(item.path);
-              } else if (item.type === 'dir') {
-                  const subFiles = await getAllFiles(octokit, owner, repo, item.path);
-                  files = files.concat(subFiles);
-              }
-          }
-      
-          return files;
-        }
-      
+              
         const allFiles = await getAllFiles(octokit, context.repo.owner, context.repo.repo, 'src');
 
-        const filesWithTests = allFiles
-            .filter(filename => filename.endsWith('.spec.ts'))``
+        const allFilesWithTests = allFiles
+            .filter(filename => filename.endsWith('.spec.ts'))
             .map(testFile => testFile.replace('.spec.ts', '.ts'));
-        
-        console.log(filesWithTests);
+        console.log(filesWithTests);        
 
-        const filesToStryke = filesWithTests.slice(0, previousCount+1);
-  
-        if (filesToStryke.length > 0 && filesToStryke.length === filesToStryke.length) {
-            core.exportVariable('COMPONENT_COUNT', 'complete');
-        } else {
-            core.exportVariable('COMPONENT_FILES', componentsToStryke.join(', '));
+        if (allFilesWithTests.length() === 0) {
+            core.exportVariable('COMPONENT_FILES', '');
+
+        } else if (allFilesWithTests.length() > previousCount) {
+            const filesToStryke = allFilesWithTests.slice(0, previousCount+1);
+            core.exportVariable('COMPONENT_FILES', filesToStryke.join(', '));
             core.exportVariable('COMPONENT_COUNT', previousCount+1);
-        }
 
+        } else if (allFilesWithTests.length() === previousCount) {
+            core.exportVariable('COMPONENT_FILES', allFilesWithTests.join(', '));
+        }
+  
     } catch (error) {
         core.setFailed(error.message);
     }
   }
 
 (async () => {
-  try {
-    const runId = await getLatestSuccessfulRun();
-    if (!runId) {
-      console.log('No successful workflow run found');
-      return;
+    try {
+        const runId = await getLatestSuccessfulRun();
+        const artifactId = await getArtifactId(runId);
+        const zipPath = await downloadArtifact(artifactId);
+        const extractPath = extractArtifact(zipPath);
+        const artifactFilePath = path.join(extractPath, 'stryker-incremental.json');
+        
+        if (!fs.existsSync(artifactFilePath)) {
+          console.log(`Artifact file ${artifactFilePath} not found`);
+        }
+        console.log(`Artifact file found at: ${artifactFilePath}`);
+
+        getComponentFiles();
+
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
     }
-
-    const artifactId = await getArtifactId(runId);
-    if (!artifactId) {
-      console.log(`Artifact ${ARTIFACT_NAME} not found`);
-      return;
-    }
-
-    const zipPath = await downloadArtifact(artifactId);
-    console.log(`Artifact downloaded to: ${zipPath}`);
-
-    const extractPath = extractArtifact(zipPath);
-    console.log(`Artifact extracted to: ${extractPath}`);
-
-    const artifactFilePath = path.join(extractPath, 'stryker-incremental.json');
-    if (!fs.existsSync(artifactFilePath)) {
-      console.log(`Artifact file ${artifactFilePath} not found`);
-      return;
-    }
-    console.log(`Artifact file found at: ${artifactFilePath}`);
-
-    getComponentFiles();
-
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
-  }
 })();
